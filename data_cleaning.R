5f47dc95af39c5c637df89f4487ec3598a3cc45f
Lissa <c.j.vanlissa@uu.nl>
2019-09-23 19:35:08 +0200

    Initialize project

---
+++ data_cleaning.R
@@ -0,0 +1,30 @@
+if(!require(checkpoint)) install.packages("checkpoint")
+checkpoint("2019-09-23")
+if(!require(foreign)) install.packages("foreign")
+if(!require(psych)) install.packages("psych")
+if(!require(devtools)) install.packages("devtools")
+if(!require(motley)) install_github("cjvanlissa/motley")
+
+spss_data <- read.spss("Meta-codebook_23-09-2019.sav", to.data.frame = TRUE)
+spss_data[spss_data == 999] <- NA
+
+
+descriptives <- describe(spss_data)
+write.csv(descriptives, "spss_data_descriptives.csv", row.names = FALSE)
+
+# Note: Many variables with only one value. Remove these.
+spss_data <- spss_data[, !descriptives$sd == 0]
+
+# Check for variables with only missing data
+which(colSums(is.na(spss_data)) == max(colSums(is.na(spss_data))))
+table(spss_data$B)
+# Seems not to be a problem
+
+# Check for cases with only missing data
+head(spss_data[rowSums(is.na(spss_data)) == max(rowSums(is.na(spss_data))), ])
+spss_data <- spss_data[!rowSums(is.na(spss_data)) == max(rowSums(is.na(spss_data))), ]
+
+
+# Save to file ------------------------------------------------------------
+
+write.csv(spss_data, "data.csv", row.names = FALSE)
