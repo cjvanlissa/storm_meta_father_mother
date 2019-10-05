138276ea0ebb9b20ffd8b3c1a9f684b44fea81d8
Lissa <c.j.vanlissa@uu.nl>
2019-10-05 18:14:46 +0200

    Preprocessed data

---
+++ data_preprocessing.R
@@ -0,0 +1,53 @@
+df <- read.csv("data.csv", stringsAsFactors = FALSE)
+
+
+# Drop rows ---------------------------------------------------------------
+# Drop non-eligible
+df <- df[df$Eligible == "yes", ]
+# Drop cases with max missingness
+missing <- rowSums(is.na(df))
+df <- df[!missing == max(missing), ]
+# Drop missing ID_P
+df <- df[!is.na(df$ID_P), ]
+
+run_length <- rle(as.vector(df$ID_P))
+all(run_length$lengths[seq(1, length(run_length$lengths), by = 2)]== run_length$lengths[seq(2, length(run_length$lengths), by = 2)])
+
+
+# Create unique merge id --------------------------------------------------
+
+df$id_merge <- gsub(" ", "", apply(df[, grep("^ID(?!_P$)", names(df), value = TRUE, perl = TRUE)], 1, paste, collapse = "_"))
+
+#df <- df[!is.na(df$id_merge), ]
+
+tmp <- table(df$ID_P, df$id_merge)
+
+double_ids <- colnames(tmp)[apply(tmp, 2, sum) == 2]
+single_ids <- colnames(tmp)[apply(tmp, 2, sum) == 1]
+
+#tmp <- df[df$id_merge %in% single_ids, ]
+
+#tmp <- df[df$id_merge %in% single_ids, c(grep("^ID(?!_P$)", names(df), value = TRUE, perl = TRUE), "ID_P")]
+
+df$id_merge[df$id_merge %in% single_ids] <- gsub("(F-F|F-|father-|M-M|M-|mother-)", "", df$id_merge[df$id_merge %in% single_ids])
+
+# Merge data --------------------------------------------------------------
+
+df_mother <- df[df$ID_P == "mother", ]
+df_father <- df[df$ID_P == "father", ]
+
+# Check if all unique
+any(duplicated(df_father$id_merge))
+any(duplicated(df_mother$id_merge))
+# Check if all in order
+if(!all(diff(match(df_father$id_merge, df_mother$id_merge)) == 1)) stop("Difference in order of rows between mother and father")
+
+df_mother$R_f <- df_father$R
+df_mother$R_m_f_check <- df_father$R_m_f
+
+if(!all(na.omit(rowSums(cbind(df_mother$R_m_f, -1*df_mother$R_m_f_check))) == 0)){
+  df_mother[!rowSums(cbind(df_mother$R_m_f, -1*df_mother$R_m_f_check)) == 0& !is.na(rowSums(cbind(df_mother$R_m_f, -1*df_mother$R_m_f_check))), c("R_m_f", "R_m_f_check", "Authors", "Pub_Year") ]
+  stop("R_m_f was different between the mother and father rows")
+}
+
+write.csv(df_mother, "data_cleaned.csv", row.names = FALSE)
