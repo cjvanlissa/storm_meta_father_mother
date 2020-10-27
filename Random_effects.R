99682dbb183035753709b2b3ec894316a01f86eb
lisavanderstorm <vanderstorm@essb.eur.nl>
2020-10-27 17:12:08 +0100

    Update table for random effects including the heterogeneity statistics

--- Random_effects.R
+++ Random_effects.R
@@ -1,8 +1,7 @@
-
+library(dplyr)
 source("pool_correlation_matrices_robust_se.R")
 set.seed(3990)
 pool_correlation_matrices()
-
 #to be able to run the following sPIipt, it is necessary to run the sPIipt "pool_correlation_matrices_robust_se.R" seperately for each moderator!"
 
 #First, I load all the dataframes needed for the random effects analyses, and recode which_cor in a way that it is clear for which subgroup the effect size is.
@@ -58,10 +57,9 @@ t1<-data.frame(t(sapply(unique(dat$which_corR), function(x){
   predict(res.REtotal)
 })))
 t2<-data.frame(t(sapply(unique(dat$which_corR),function(x){res.REtotal <- rma(yi, vi, data=dat[dat$which_corR == x, ], digits=3)})))
-t<-cbind(t1[,"pred"], t1[,"se"], t2[,"pval"], t1[,"ci.lb"], t1[,"ci.ub"], t1[,"PI.lb"], t1[,"PI.ub"]) 
-colnames(t) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-#heterogeneity:
-Q1<- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat)
+t<-cbind(t1[,"pred"], t1[,"se"], t2[,"pval"], t1[,"ci.lb"], t1[,"ci.ub"], t1[,"cr.lb"], t1[,"cr.ub"], t2[,"tau2"], t2[,"I2"], t2[,"QE"], t2[, "QEp"]) 
+colnames(t) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #Random effects model including prediction intervals for each parenting dimension, with moderator of parents' sex. 
 # behavioral control (recoded to positive behavioral control): 
 tc1<-data.frame(t(sapply(unique(dat_c$which_corR), function(x){ 
@@ -71,9 +69,8 @@ tc1<-data.frame(t(sapply(unique(dat_c$which_corR), function(x){
 tc2<-data.frame(t(sapply(unique(dat_c$which_corR), function(x){ 
   res.REcontrol <- rma(yi, vi, data=dat_c[dat_c$which_corR == x, ], digits=3)
 })))
-tc<-cbind(tc1[,"pred"], tc1[,"se"], tc2[,"pval"], tc1[,"ci.lb"], tc1[,"ci.ub"], tc1[,"PI.lb"], tc1[,"PI.ub"]) 
-colnames(tc) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q2a <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_c)
+tc<-cbind(tc1[,"pred"], tc1[,"se"], tc2[,"pval"], tc1[,"ci.lb"], tc1[,"ci.ub"], tc1[,"cr.lb"], tc1[,"cr.ub"], tc2[,"tau2"], tc2[,"I2"], tc2[,"QE"], tc2[, "QEp"]) 
+colnames(tc) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
 
 #parental warmth:
 tw1<-data.frame(t(sapply(unique(dat_w$which_corR), function(x){ 
@@ -83,9 +80,8 @@ tw1<-data.frame(t(sapply(unique(dat_w$which_corR), function(x){
 tw2<-data.frame(t(sapply(unique(dat_w$which_corR), function(x){ 
   res.REwarm <- rma(yi, vi, data=dat_w[dat_w$which_corR == x, ], digits=3)
 })))
-tw<-cbind(tw1[,"pred"], tw1[,"se"], tw2[,"pval"], tw1[,"ci.lb"], tw1[,"ci.ub"], tw1[,"PI.lb"], tw1[,"PI.ub"]) 
-colnames(tw) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q2b <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_w)
+tw<-cbind(tw1[,"pred"], tw1[,"se"], tw2[,"pval"], tw1[,"ci.lb"], tw1[,"ci.ub"], tw1[,"cr.lb"], tw1[,"cr.ub"], tw2[,"tau2"], tw2[,"I2"], tw2[,"QE"], tw2[, "QEp"]) 
+colnames(tw) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
 #(low) harsh parenting:
 th1<-data.frame(t(sapply(unique(dat_h$which_corR), function(x){ 
   res.REharsh <- rma(yi, vi, data=dat_h[dat_h$which_corR == x, ], digits=3)
@@ -94,44 +90,9 @@ th1<-data.frame(t(sapply(unique(dat_h$which_corR), function(x){
 th2<-data.frame(t(sapply(unique(dat_h$which_corR), function(x){ 
   res.REharsh <- rma(yi, vi, data=dat_h[dat_h$which_corR == x, ], digits=3)
 })))
-th<-cbind(th1[,"pred"], th1[,"se"], th2[,"pval"], th1[,"ci.lb"], th1[,"ci.ub"], th1[,"PI.lb"], th1[,"PI.ub"]) 
-colnames(th) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q2c <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_h)
+th<-cbind(th1[,"pred"], th1[,"se"], th2[,"pval"], th1[,"ci.lb"], th1[,"ci.ub"], th1[,"cr.lb"], th1[,"cr.ub"], th2[,"tau2"], th2[,"I2"], th2[,"QE"], th2[, "QEp"]) 
+colnames(th) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
 
-#Random effects model for studies examining specific types parental control, moderator of parents' sex. 
-#behavioral control
-tcb1<-data.frame(t(sapply(unique(dat_cb$which_corR), function(x){ 
-  res.REbcontrol <- rma(yi, vi, data=dat_cb[dat_cb$which_corR == x, ], digits=3)
-  predict(res.REbcontrol)
-})))
-tcb2<-data.frame(t(sapply(unique(dat_cb$which_corR), function(x){ 
-  res.REbcontrol <- rma(yi, vi, data=dat_cb[dat_cb$which_corR == x, ], digits=3)
-})))
-tcb<-cbind(tcb1[,"pred"], tcb1[,"se"], tcb2[,"pval"], tcb1[,"ci.lb"], tcb1[,"ci.ub"], tcb1[,"PI.lb"], tcb1[,"PI.ub"]) 
-colnames(tcb) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q3a <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_cb)
-#inconsistent control
-tci1<-data.frame(t(sapply(unique(dat_ci$which_corR), function(x){ 
-  res.REicontrol <- rma(yi, vi, data=dat_ci[dat_ci$which_corR == x, ], digits=3)
-  predict(res.REicontrol)
-})))
-tci2<-data.frame(t(sapply(unique(dat_ci$which_corR), function(x){ 
-  res.REicontrol <- rma(yi, vi, data=dat_ci[dat_ci$which_corR == x, ], digits=3)
-})))
-tci<-cbind(tci1[,"pred"], tci1[,"se"], tci2[,"pval"], tci1[,"ci.lb"], tci1[,"ci.ub"], tci1[,"PI.lb"], tci1[,"PI.ub"]) 
-colnames(tci) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q3b <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_ci)
-#psychological control
-tcp1<-data.frame(t(sapply(unique(dat_cp$which_corR), function(x){ 
-  res.REpcontrol <- rma(yi, vi, data=dat_cp[dat_cp$which_corR == x, ], digits=3)
-  predict(res.REpcontrol)
-})))
-tcp2<-data.frame(t(sapply(unique(dat_cp$which_corR), function(x){ 
-  res.REpcontrol <- rma(yi, vi, data=dat_cp[dat_cp$which_corR == x, ], digits=3)
-})))
-tcp<-cbind(tcp1[,"pred"], tcp1[,"se"], tcp2[,"pval"], tcp1[,"ci.lb"], tcp1[,"ci.ub"], tcp1[,"PI.lb"], tcp1[,"PI.ub"]) 
-colnames(tcp) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q3c <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_cp)
 #Random effects model for studies examining concurrent vs predictive associations, moderator of parents' sex. 
 #concurrent
 tconc1<-data.frame(t(sapply(unique(dat_concurrent$which_corR), function(x){ 
@@ -141,9 +102,9 @@ tconc1<-data.frame(t(sapply(unique(dat_concurrent$which_corR), function(x){
 tconc2<-data.frame(t(sapply(unique(dat_concurrent$which_corR), function(x){ 
   res.REconcurrent <- rma(yi, vi, data=dat_concurrent[dat_concurrent$which_corR == x, ], digits=3)
 })))
-tconc<-cbind(tconc1[,"pred"], tconc1[,"se"], tconc2[,"pval"], tconc1[,"ci.lb"], tconc1[,"ci.ub"], tconc1[,"PI.lb"], tconc1[,"PI.ub"]) 
-colnames(tconc) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q4a <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_concurrent)
+tconc<-cbind(tconc1[,"pred"], tconc1[,"se"], tconc2[,"pval"], tconc1[,"ci.lb"], tconc1[,"ci.ub"], tconc1[,"cr.lb"], tconc1[,"cr.ub"], tconc2[,"tau2"], tconc2[,"I2"], tconc2[,"QE"], tconc2[, "QEp"]) 
+colnames(tconc) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #predictive
 tpred1<-data.frame(t(sapply(unique(dat_predictive$which_corR), function(x){ 
   res.REpredictive <- rma(yi, vi, data=dat_predictive[dat_predictive$which_corR == x, ], digits=3)
@@ -152,9 +113,9 @@ tpred1<-data.frame(t(sapply(unique(dat_predictive$which_corR), function(x){
 tpred2<-data.frame(t(sapply(unique(dat_predictive$which_corR), function(x){ 
   res.REpredictive <- rma(yi, vi, data=dat_predictive[dat_predictive$which_corR == x, ], digits=3)
 })))
-tpred<-cbind(tpred1[,"pred"], tpred1[,"se"], tpred2[,"pval"], tpred1[,"ci.lb"], tpred1[,"ci.ub"], tpred1[,"PI.lb"], tpred1[,"PI.ub"]) 
-colnames(tpred) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q4b <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_predictive)
+tpred<-cbind(tpred1[,"pred"], tpred1[,"se"], tpred2[,"pval"], tpred1[,"ci.lb"], tpred1[,"ci.ub"], tpred1[,"cr.lb"], tpred1[,"cr.ub"], tpred2[,"tau2"], tpred2[,"I2"], tpred2[,"QE"], tpred2[, "QEp"]) 
+colnames(tpred) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #Random effects model for studies examining pre-adolescent vs adolescent children, moderator of parents' sex. 
 #young (pre-adolescence)
 tyoung1<-data.frame(t(sapply(unique(dat_young$which_corR), function(x){ 
@@ -164,9 +125,9 @@ tyoung1<-data.frame(t(sapply(unique(dat_young$which_corR), function(x){
 tyoung2<-data.frame(t(sapply(unique(dat_young$which_corR), function(x){ 
   res.REyoung <- rma(yi, vi, data=dat_young[dat_young$which_corR == x, ], digits=3)
 })))
-tyoung<-cbind(tyoung1[,"pred"], tyoung1[,"se"], tyoung2[,"pval"], tyoung1[,"ci.lb"], tyoung1[,"ci.ub"], tyoung1[,"PI.lb"], tyoung1[,"PI.ub"]) 
-colnames(tyoung) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q5a <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_young)
+tyoung<-cbind(tyoung1[,"pred"], tyoung1[,"se"], tyoung2[,"pval"], tyoung1[,"ci.lb"], tyoung1[,"ci.ub"], tyoung1[,"cr.lb"], tyoung1[,"cr.ub"], tyoung2[,"tau2"], tyoung2[,"I2"], tyoung2[,"QE"], tyoung2[, "QEp"]) 
+colnames(tyoung) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #old (adolescence)
 told1<-data.frame(t(sapply(unique(dat_old$which_corR), function(x){ 
   res.REold <- rma(yi, vi, data=dat_old[dat_old$which_corR == x, ], digits=3)
@@ -175,9 +136,9 @@ told1<-data.frame(t(sapply(unique(dat_old$which_corR), function(x){
 told2<-data.frame(t(sapply(unique(dat_old$which_corR), function(x){ 
   res.REold <- rma(yi, vi, data=dat_old[dat_old$which_corR == x, ], digits=3)
 })))
-told<-cbind(told1[,"pred"], told1[,"se"], told2[,"pval"], told1[,"ci.lb"], told1[,"ci.ub"], told1[,"PI.lb"], told1[,"PI.ub"]) 
-colnames(told) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q5b <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_old)
+told<-cbind(told1[,"pred"], told1[,"se"], told2[,"pval"], told1[,"ci.lb"], told1[,"ci.ub"], told1[,"cr.lb"], told1[,"cr.ub"], told2[,"tau2"], told2[,"I2"], told2[,"QE"], told2[, "QEp"]) 
+colnames(told) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #Random effects model for studies examining differentiation of 3 age-groups for reviewer 1, moderator of parents' sex. 
 #young
 t3young1<-data.frame(t(sapply(unique(dat_3young$which_corR), function(x){ 
@@ -187,9 +148,9 @@ t3young1<-data.frame(t(sapply(unique(dat_3young$which_corR), function(x){
 t3young2<-data.frame(t(sapply(unique(dat_3young$which_corR), function(x){ 
   res.RE3young <- rma(yi, vi, data=dat_3young[dat_3young$which_corR == x, ], digits=3)
 })))
-t3young<-cbind(t3young1[,"pred"], t3young1[,"se"], t3young2[,"pval"], t3young1[,"ci.lb"], t3young1[,"ci.ub"], t3young1[,"PI.lb"], t3young1[,"PI.ub"]) 
-colnames(t3young) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q6a <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_3young)
+t3young<-cbind(t3young1[,"pred"], t3young1[,"se"], t3young2[,"pval"], t3young1[,"ci.lb"], t3young1[,"ci.ub"], t3young1[,"cr.lb"], t3young1[,"cr.ub"], t3young2[,"tau2"], t3young2[,"I2"], t3young2[,"QE"], t3young2[, "QEp"]) 
+colnames(t3young) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #medium age
 t3med1<-data.frame(t(sapply(unique(dat_3med$which_corR), function(x){ 
   res.RE3med <- rma(yi, vi, data=dat_3med[dat_3med$which_corR == x, ], digits=3)
@@ -198,9 +159,9 @@ t3med1<-data.frame(t(sapply(unique(dat_3med$which_corR), function(x){
 t3med2<-data.frame(t(sapply(unique(dat_3med$which_corR), function(x){ 
   res.RE3med <- rma(yi, vi, data=dat_3med[dat_3med$which_corR == x, ], digits=3)
 })))
-t3med<-cbind(t3med1[,"pred"], t3med1[,"se"], t3med2[,"pval"], t3med1[,"ci.lb"], t3med1[,"ci.ub"], t3med1[,"PI.lb"], t3med1[,"PI.ub"]) 
-colnames(t3med) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q6b <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_3med)
+t3med<-cbind(t3med1[,"pred"], t3med1[,"se"], t3med2[,"pval"], t3med1[,"ci.lb"], t3med1[,"ci.ub"], t3med1[,"cr.lb"], t3med1[,"cr.ub"], t3med2[,"tau2"], t3med2[,"I2"], t3med2[,"QE"], t3med2[, "QEp"]) 
+colnames(t3med) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #old 
 t3old1<-data.frame(t(sapply(unique(dat_3old$which_corR), function(x){ 
   res.RE3old <- rma(yi, vi, data=dat_3old[dat_3old$which_corR == x, ], digits=3)
@@ -209,9 +170,9 @@ t3old1<-data.frame(t(sapply(unique(dat_3old$which_corR), function(x){
 t3old2<-data.frame(t(sapply(unique(dat_3old$which_corR), function(x){ 
   res.RE3old <- rma(yi, vi, data=dat_3old[dat_3old$which_corR == x, ], digits=3)
 })))
-t3old<-cbind(t3old1[,"pred"], t3old1[,"se"], t3old2[,"pval"], t3old1[,"ci.lb"], t3old1[,"ci.ub"], t3old1[,"PI.lb"], t3old1[,"PI.ub"]) 
-colnames(t3old) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q6c <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_3old)
+t3old<-cbind(t3old1[,"pred"], t3old1[,"se"], t3old2[,"pval"], t3old1[,"ci.lb"], t3old1[,"ci.ub"], t3old1[,"cr.lb"], t3old1[,"cr.ub"], t3old2[,"tau2"], t3old2[,"I2"], t3old2[,"QE"], t3old2[, "QEp"]) 
+colnames(t3old) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 
 #Random effects model for non-US vs. US (for Reviewer 1)
 #US
@@ -222,9 +183,9 @@ tUS1<-data.frame(t(sapply(unique(dat_US$which_corR), function(x){
 tUS2<-data.frame(t(sapply(unique(dat_US$which_corR), function(x){ 
   res.REUS <- rma(yi, vi, data=dat_US[dat_US$which_corR == x, ], digits=3)
 })))
-tUS<-cbind(tUS1[,"pred"], tUS1[,"se"], tUS2[,"pval"], tUS1[,"ci.lb"], tUS1[,"ci.ub"], tUS1[,"PI.lb"], tUS1[,"PI.ub"]) 
-colnames(tUS) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q7a <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_US)
+tUS<-cbind(tUS1[,"pred"], tUS1[,"se"], tUS2[,"pval"], tUS1[,"ci.lb"], tUS1[,"ci.ub"], tUS1[,"cr.lb"], tUS1[,"cr.ub"], tUS2[,"tau2"], tUS2[,"I2"], tUS2[,"QE"], tUS2[, "QEp"]) 
+colnames(tUS) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
 #non-US 
 tnonUS1<-data.frame(t(sapply(unique(dat_nonUS$which_corR), function(x){ 
   res.REnonUS <- rma(yi, vi, data=dat_nonUS[dat_nonUS$which_corR == x, ], digits=3)
@@ -233,11 +194,11 @@ tnonUS1<-data.frame(t(sapply(unique(dat_nonUS$which_corR), function(x){
 tnonUS2<-data.frame(t(sapply(unique(dat_nonUS$which_corR), function(x){ 
   res.REnonUS <- rma(yi, vi, data=dat_nonUS[dat_nonUS$which_corR == x, ], digits=3)
 })))
-tnonUS<-cbind(tnonUS1[,"pred"], tnonUS1[,"se"], tnonUS2[,"pval"], tnonUS1[,"ci.lb"], tnonUS1[,"ci.ub"], tnonUS1[,"PI.lb"], tnonUS1[,"PI.ub"]) 
-colnames(tnonUS) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub")
-Q7b <- rma(yi, vi, mods=~ factor(which_corR)-1, data=dat_nonUS)
-#Merge al results table into one table
-res.RE<-data.frame(rbind(t, tc, tw, th, tcb, tci, tcp, tconc, tpred, tyoung, told, t3young, t3med, t3old, tUS, tnonUS))
+tnonUS<-cbind(tnonUS1[,"pred"], tnonUS1[,"se"], tnonUS2[,"pval"], tnonUS1[,"ci.lb"], tnonUS1[,"ci.ub"], tnonUS1[,"cr.lb"], tnonUS1[,"cr.ub"], tnonUS2[,"tau2"], tnonUS2[,"I2"], tnonUS2[,"QE"], tnonUS2[, "QEp"]) 
+colnames(tnonUS) <- c("Estimate", "SE", "P value", "CI lb", "CI ub", "PI lb", "PI ub", "Tau2", "I2", "QE", "QEp")
+
+#Merge al results table into one table (without three age subgroups US/nonUS as we don't publish this but only mention in response to reviewer letter)
+res.RE<-data.frame(rbind(t, tc, tw, th, tconc, tpred, tyoung, told))
 #remove unnecessary columns, could not come up with a better way;)
 #Unlist, again, could not find a better way to do this
 res.RE$Estimate<-unlist(res.RE$Estimate, recursive=TRUE, use.names=TRUE)
@@ -247,19 +208,12 @@ res.RE$CI.lb<-unlist(res.RE$CI.lb, recursive=TRUE, use.names=TRUE)
 res.RE$CI.ub<-unlist(res.RE$CI.ub, recursive=TRUE, use.names=TRUE)
 res.RE$PI.lb<-unlist(res.RE$PI.lb, recursive=TRUE, use.names=TRUE)
 res.RE$PI.ub<-unlist(res.RE$PI.ub, recursive=TRUE, use.names=TRUE)
-res.RE2 <- rapply(object = res.RE, f = round, classes = "numeric", how = "replace", digits = 3) 
+res.RE$Tau2<-unlist(res.RE$Tau2, recursive=TRUE, use.names=TRUE)
+res.RE$I2<-unlist(res.RE$I2, recursive=TRUE, use.names=TRUE)
+res.RE$QE<-unlist(res.RE$QE, recursive=TRUE, use.names=TRUE)
+res.RE$QEp<-unlist(res.RE$QEp, recursive=TRUE, use.names=TRUE)
+#res.RE2 <- rapply(object = res.RE, f = round, classes = "numeric", how = "replace", digits = 3) 
 
 write.csv(res.RE, "results_RE.csv", row.names = TRUE)
 
-#Make table for heterogeneity tests per subgroup
-Moderator <- c('total sample', 'parental control', "parental warmth", 'low harsh parenting', 'behavioral control', 'inconsistent control', 'psychological control', 'concurrent', 'predictive', 'young', 'old', '3young', '3med', '3old','US','non US')
-QE <- c(Q1$QE, Q2a$QE, Q2b$QE, Q2c$QE, Q3a$QE, Q3b$QE, Q3c$QE, Q4a$QE, Q4b$QE, Q5a$QE, Q5b$QE, Q6a$QE, Q6b$QE, Q6c$QE, Q7a$QE, Q7b$QE)
-#Pvalue <- c(Q1$QEp, Q2a$QEp, Q2b$QEp, Q2c$QEp, Q3a$QEp, Q3b$QEp, Q3c$QEp, Q4a$QEp, Q4b$QEp, Q5a$QEp, Q5b$QEp, Q6a$QEp, Q6b$QEp, Q6c$QEp, Q7a$QEp, Q7b$QEp)
-  #as some p's are so small that they are reported as 0, I replaced it for <.0001. However everytime I change somehting to the syntax, we should check whether this is still the same!
-Pvalue <- "< .0001"
 
-QEresults <- data.frame(Moderator, QE, Pvalue, stringsAsFactors=FALSE)
-QEresults2 <- rapply(object = QEresults, f = round, classes = "numeric", how = "replace", digits = 4) 
-?rapply
-?write.csv
-write.csv(QEresults2, "results_QE.csv", row.names = FALSE)
